565f1607316e78463df3285dcb795385002ad973
Ibrahim H <mohammad.ihakash@gmail.com>
2022-01-03 16:38:05 +0600

    non-serializable redux fixed

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../features/userSlice'
+import { getDefaultMiddleware } from '@reduxjs/toolkit'
+
 
 export default configureStore({
     reducer: {
         user: userReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 })
