163369296a66593936ea887d0f1db621fa2b0930
Ibrahim H <mohammad.ihakash@gmail.com>
2022-01-06 03:14:52 +0600

    dashboard my courses list

--- src/components/Dashboard/MyOrder.js
+++ src/components/Dashboard/MyOrder.js
@@ -7,6 +7,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
+import { selectcourses } from '../../features/coursesSlice';
+import { CircleNotificationsTwoTone } from '@mui/icons-material';
 
 const MyOrder = () => {
     // const { user, token } = useAuth();
@@ -37,9 +41,26 @@ const MyOrder = () => {
 
     // console.log(orderList);
     // console.log(orderList);
+    const user = useSelector(selectUser);
+    const [enroll, setEnroll] = React.useState({});
+    const courses = useSelector(selectcourses);
+    const [currentCourses, setCurrentCourses] = React.useState([]);
+    React.useEffect(() => {
+        fetch(`http://localhost:5000/users/${user?.email}`)
+            .then(res => res.json())
+            .then(data => setEnroll(data?.enroll))
+    }, [user])
+    React.useEffect(() => {
+        const newData = [];
+        for (const item in enroll) {
+            newData.push(courses.find(p => p._id === item));
+        }
+        setCurrentCourses(newData);
+    }, [enroll, courses])
+    console.log(currentCourses);
     return (
         <>
-            <Typography variant="h5" sx={{ my: 2 }}>My courses: {`5`}</Typography>
+            <Typography variant="h5" sx={{ my: 2 }}>My courses: {currentCourses.length}</Typography>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
@@ -50,18 +71,18 @@ const MyOrder = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {/* {orderList.map((order) => (
+                        {currentCourses.map((order) => (
                             <TableRow
                                 key={order?._id}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell component="th" scope="order?">{order?.product?.name}</TableCell>
-                                <TableCell align="left">{order?.status}</TableCell>
+                                <TableCell component="th" scope="order?">{order?.classname}</TableCell>
+                                <TableCell align="left">{order?.schedule}</TableCell>
                                 <TableCell align="left">
-                                    <Button onClick={() => handleCancelOrder(order._id)}>Cancel</Button>
+                                    <Button>Cancel</Button>
                                 </TableCell>
                             </TableRow>
-                        ))} */}
+                        ))}
                     </TableBody>
                 </Table>
             </TableContainer>
